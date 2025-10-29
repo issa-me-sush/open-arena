24b6378b2bdf50b4044c498e1532521e23879ff2
sushthecoda <coolsushaan@gmail.com>
2025-10-29 17:06:54 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "OpenServ Arena",
   description: "OpenServ Agent Arena",
+icons: {
+    icon: "/openservlogolight.svg",
+  },
 };
 
 export default function RootLayout({
